e0d150c19659fcd19bebdff9f8667a693c35f7f5
Najoung Kim <nk3351@gr003.hpc.nyu.edu>
2022-04-27 00:20:20 -0400

    COGS Reformatting scripts.

---
+++ scripts/reformat_datasets.sh
@@ -0,0 +1,2 @@
+python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/w_n
+python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_str --new_heldout_type random_str
