09f8a8301020fb59d61bf3c7bcfaee4ae0fccf21
najoungkim <najoungk@gmail.com>
2022-04-27 21:31:28 -0400

    Added .json conversion script to format data into parsing-transformers format.

--- scripts/reformat_datasets.sh
+++ scripts/reformat_datasets.sh
@@ -1,2 +1,5 @@
 python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/w_n
 python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_str --new_heldout_type random_str
+
+python ../utils/convert_cogs_to_json.py --input_path ../data/w_n/ --output_path ../data/w_n
+python ../utils/convert_cogs_to_json.py --input_path ../data/random_str/ --output_path ../data/random_str
