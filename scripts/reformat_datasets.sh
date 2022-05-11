9ae97fd277b71beb0318f7ac5b3a82b60095a22a
najoungkim <najoungk@gmail.com>
2022-05-11 10:54:15 -0400

    Made reduced consonant set a separate option.

--- scripts/reformat_datasets.sh
+++ scripts/reformat_datasets.sh
@@ -1,11 +1,16 @@
 python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/w_n
 python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_str --new_heldout_type random_str --seed 555
 python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_str_shorter --new_heldout_type random_str_shorter --seed 666
-python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_phonological_str --new_heldout_type random_phonological_str --seed 777
-python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_phonological_str_shorter --new_heldout_type random_phonological_str_shorter --seed 888
+python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_cvcv_str --new_heldout_type random_cvcv_str --seed 777
+python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_cvcv_str_shorter --new_heldout_type random_cvcv_str_shorter --seed 888
+python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_cvcv_str_cons_reduced --new_heldout_type random_cvcv_str_cons_reduced --seed 999
+python ../utils/reformat_lexical_heldouts.py --input_path ../../COGS/data/ --output_path ../data/random_cvcv_str_cons_reduced_shorter --new_heldout_type random_cvcv_str_cons_reduced_shorter --seed 9999
+
 
 python ../utils/convert_cogs_to_json.py --input_path ../data/w_n/ --output_path ../data/w_n
 python ../utils/convert_cogs_to_json.py --input_path ../data/random_str/ --output_path ../data/random_str
 python ../utils/convert_cogs_to_json.py --input_path ../data/random_str_shorter/ --output_path ../data/random_str_shorter
-python ../utils/convert_cogs_to_json.py --input_path ../data/random_phonological_str --output_path ../data/random_phonological_str
-python ../utils/convert_cogs_to_json.py --input_path ../data/random_phonological_str_shorter --output_path ../data/random_phonological_str_shorter
+python ../utils/convert_cogs_to_json.py --input_path ../data/random_cvcv_str --output_path ../data/random_cvcv_str
+python ../utils/convert_cogs_to_json.py --input_path ../data/random_cvcv_str_shorter --output_path ../data/random_cvcv_str_shorter
+python ../utils/convert_cogs_to_json.py --input_path ../data/random_cvcv_str_cons_reduced --output_path ../data/random_cvcv_str_cons_reduced
+python ../utils/convert_cogs_to_json.py --input_path ../data/random_cvcv_str_cons_reduced_shorter --output_path ../data/random_cvcv_str_cons_reduced_shorter
